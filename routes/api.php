b078b114674a9aa26d0ada7fcb88c1166b1d9634
Alex <alex@admasoft.com>
2025-03-02 17:58:26 +0100

    Controllers Models & Resources

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,23 @@
 <?php
 
-// use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostsController;
 use App\Http\Controllers\AuthController;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
 
-Route::get('/posts', [PostsController::class, 'index']);
-Route::get('/posts/{post}', [PostsController::class, 'show']);
-Route::post('/posts', [PostsController::class, 'store']);
-Route::put('/posts/{post}', [PostsController::class, 'update']);
-Route::delete('/posts/{post}', [PostsController::class, 'destroy']);
-
-
+# rutas de la API publicas 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/posts', [PostsController::class, 'index']);
+Route::get('/posts/{post}', [PostsController::class, 'show']);
+
+# rutas de la API protegidas
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/posts', [PostsController::class, 'store']);
+    Route::put('/posts/{post}', [PostsController::class, 'update']);
+    Route::delete('/posts/{post}', [PostsController::class, 'destroy']);
+});
+
+
+
